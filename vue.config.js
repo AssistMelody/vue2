23bb06c989010c75ad38100ec21208d7f6d7d9d0
yuan.shen <yuan.shen@marketin.cn>
2024-07-22 14:38:23 +0800

    feat: 添加lint

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+const wasmFile = path.resolve(__dirname, 'node_modules/mediainfo.js/dist/MediaInfoModule.wasm')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: wasmFile, to: '.' }]
+      })
+    ]
+  }
 })
